fc5fcf98d96496819606bd9f329c6f0d8b85b4a6
Phuc <phucbopbnation@gmail.com>
2023-03-07 22:06:19 +0700

    init

---
+++ src/main/java/com/mywebapp/usermanaweb/UserManaWebApplication.java
@@ -0,0 +1,13 @@
+package com.mywebapp.usermanaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManaWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mywebapp/usermanaweb/UserManaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywebapp.usermanaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
