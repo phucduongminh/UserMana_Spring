08634739d3171a0878ddf4493d99cec85a09c2f8
Phuc <phucbopbnation@gmail.com>
2023-03-07 23:08:03 +0700

    testHome

---
+++ src/main/java/com/mywebapp/usermanaweb/HomeController.java
@@ -0,0 +1,13 @@
+package com.mywebapp.usermanaweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("")
+    public String HomePage (){
+        return "index";
+    }
+}
